89fbd2b6468f9f166f266023d4d0f50bb51bc3fe
JetsadaWijit <56456776+JetsadaWijit@users.noreply.github.com>
2024-01-30 18:46:35 +0700

    Change path style

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
 // main page
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port);

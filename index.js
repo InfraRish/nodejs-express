a0676379c95e19a45fdc4fd5715c4dcb6317271b
JetsadaWijit <56456776+JetsadaWijit@users.noreply.github.com>
2024-01-30 18:40:34 +0700

    set up html

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+
+// main page
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(port);

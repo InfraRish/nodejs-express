f8331254b14574c474391087e25b98fe2069df32
JetsadaWijit <56456776+JetsadaWijit@users.noreply.github.com>
2024-01-30 18:57:10 +0700

    Change logic access to main page to static

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// main page
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(port);

11324de0e105cba264524e59b27d6830130fca40
JetsadaWijit <Ze_ro_owen@hotmail.com>
2024-05-19 11:20:53 +0000

    import dependencies

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
 const express = require('express');
+const session = require('express-session');
+const bcrypt = require('bcrypt'); // use for hash password
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 

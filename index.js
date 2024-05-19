765964733ab5332f57e110b4e9bb4f4c637aaf94
JetsadaWijit <Ze_ro_owen@hotmail.com>
2024-05-19 12:28:00 +0000

    del unuse

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const session = require('express-session');
 const bcrypt = require('bcrypt'); // use for hash password
 const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));

1bf6b02b3ac4f60a9faa13755247efb723ea989a
JetsadaWijit <Ze_ro_owen@hotmail.com>
2024-05-19 11:17:17 +0000

    Create ejs

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 require('dotenv').config();
 const express = require('express');
-const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Set static folder
-app.use(express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Configure session
+app.use(session({
+    secret: 'abc', // Replace with your secret key
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false } // Set to true if using HTTPS
+}));
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('home', { title: 'Home' });
+});
 
 app.listen(port);
